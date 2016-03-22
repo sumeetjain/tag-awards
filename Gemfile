1cf941c353c51740c12cc57a696e849a4a4b840c
Melinda Deinert <melinda.deinert@gmail.com>
2016-03-22 14:29:21 -0500

    Installed

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source "https://rubygems.org"
 
 ruby "2.3.0"
 
+gem "administrate", "~> 0.1.4"
 gem "autoprefixer-rails"
 gem "delayed_job_active_record"
 gem 'devise'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   
+  namespace :admin do
+    resources :users
+
+    root to: "users#index"
+  end
+
   devise_for :users
   "home#index"
 end
