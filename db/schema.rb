e3fa2eef6e2d436274ec0593c9c6b0a481467593
Amy Wall <amymariewall@gmail.com>
2016-03-22 17:10:02 -0500

    Added columns for voter_token, first_name, last_name, admin, weight on User table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160322184556) do
+ActiveRecord::Schema.define(version: 20160322220148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,11 @@ ActiveRecord::Schema.define(version: 20160322184556) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "voter_token"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "weight"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
