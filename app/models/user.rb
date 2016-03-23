82735788aa02b3c7a3efe40c9fe8107c3208e925
Melinda Deinert <melinda.deinert@gmail.com>
2016-03-23 11:54:22 -0500

    Modified devise code to take in first and last name at sign-up

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
 end
