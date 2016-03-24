918cebb5e667699303829ae561a655b4b39380ad
Amy Wall <amymariewall@gmail.com>
2016-03-24 17:25:40 -0500

    Modified controller action for User home to get all the nominations for the current user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,11 @@ class UsersController < ApplicationController
 
 def home
   #@viewings = Viewing.where(params[:voter_token] => self.user.voter_token) #or something...ActiveRecord methods are currently escaping me.
-  @current_user = User.find_by_voter_token(params[:voter_token])
+  # @current_user = User.find_by_id(params[:user_id])
   @plays = Play.all
   @award_categories = Award.all
+  @nominations = Nomination.where({"user_id" => current_user.id})
+  binding.pry
   render "home"
 end
 
