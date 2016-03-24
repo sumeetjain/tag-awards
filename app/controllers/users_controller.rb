a53595e099b814ab2c67279265d9daa74fb8d25d
Amy Wall <amymariewall@gmail.com>
2016-03-24 17:48:13 -0500

    A user's nominations are appearing on the user dashboard.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,8 @@ class UsersController < ApplicationController
 # write methods named for each action here!
 
 def home
-  #@viewings = Viewing.where(params[:voter_token] => self.user.voter_token) #or something...ActiveRecord methods are currently escaping me.
-  # @current_user = User.find_by_id(params[:user_id])
   @plays = Play.all
   @award_categories = Award.all
-  @nominations = Nomination.where({"user_id" => current_user.id})
   binding.pry
   render "home"
 end
