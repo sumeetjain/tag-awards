5ade0a69ea07686b5ff3e1d91727ebb390dc577e
Melinda Deinert <melinda.deinert@gmail.com>
2016-03-27 11:34:35 -0500

    Form displaying correctly with new textfield naming

--- app/controllers/nominations_controller.rb
+++ app/controllers/nominations_controller.rb
@@ -6,12 +6,14 @@ class NominationsController < ActionController::Base
 
   def save_nominee
     @new_nom = Nomination.new
-    @new_nom.award_id = @award_id
-    @new_nom.nominee = params[:theater(@award_id)_1]
+    @new_nom.award_id = params[:award_id]
+    @award_id = params[:award_id]
+    @nom_count = params[:nom_count]
     @new_nom.user_id = current_user.id
-    @new_nom.role = params[:role]
-    binding.pry
+    #@new_nom.nominee = params[:current_award_data]
+    #@new_nom.role = params[:role]
     @new_nom.save
+
     redirect_to "users/home"
   end
 
