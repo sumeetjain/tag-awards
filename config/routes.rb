d8923788ed431e1823b96c65e13b7518d2d9b938
Sumeet Jain <sumeet@sumeetjain.com>
2016-03-28 16:26:22 -0500

    Remove exclusions from Devise Routes

--- config/routes.rb
+++ config/routes.rb
@@ -18,11 +18,7 @@ Rails.application.routes.draw do
 
   #{}"home#index"
 
-  devise_for :users, :skip => [:registrations]                                          
-    as :user do
-      get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'    
-      put 'users' => 'devise/registrations#update', :as => 'user_registration'
-    end
+  devise_for :users                                          
 
   get "viewings/new" => 'viewings#new'
 
