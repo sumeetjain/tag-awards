5c6bcd9715b9a96cce2bf839f9bf9c406a2fcb2b
Melinda Deinert <melinda.deinert@gmail.com>
2016-03-28 10:32:22 -0500

    While loop for five nomination option'

--- app/controllers/nominations_controller.rb
+++ app/controllers/nominations_controller.rb
@@ -5,15 +5,25 @@ class NominationsController < ActionController::Base
   end
 
   def save_nominee
-    @new_nom = Nomination.new
-    binding.pry
-    @new_nom.award_id = params[:award_id]
-    @award_id = params[:award_id]
-    @nom_count = params[:nom_count]
-    @new_nom.user_id = current_user.id
-    #@new_nom.nominee = params[:current_award_data]
-    #@new_nom.role = params[:role]
-    @new_nom.save
+    nomination_hash = {}
+
+    # 4 =>
+    #   nominations =>
+    #     [
+    #       {theater: "", show: ""},
+    #       {theater: "", show: ""},
+    #       {theater: "", show: ""},
+    #       {theater: "", show: ""},
+    #       {theater: "", show: ""}
+
+    # @new_nom = Nomination.new
+    # @new_nom.user_id = current_user.id
+    # @new_nom.award_id = params[:]
+    # @award_id = params[:]
+    # @nom_count = params[:]
+    # @new_nom.nominee = params[:]
+    # @new_nom.role = params[:]
+    # @new_nom.save
 
     redirect_to "users/home"
   end
