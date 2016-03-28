8d51ff81630468696e664cc39957fa8d91f9b5a6
Amy Wall <amymariewall@gmail.com>
2016-03-28 13:46:19 -0500

    Modified devise so users cannot register, they can only change their password.

--- config/routes.rb
+++ config/routes.rb
@@ -14,9 +14,15 @@ Rails.application.routes.draw do
   get "users/home" => 'users#home'
 
 
-  devise_for :users
+  #devise_for :users
 
-  "home#index"
+  #{}"home#index"
+
+  devise_for :users, :skip => [:registrations]                                          
+    as :user do
+      get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'    
+      put 'users' => 'devise/registrations#update', :as => 'user_registration'
+    end
 
   get "viewings/new" => 'viewings#new'
 
