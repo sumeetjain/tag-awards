f0ccdca898bb03bd7422d30804dd00d54dc5fe7e
Amy Wall <amymariewall@gmail.com>
2016-03-27 19:51:46 -0500

    Removing forms that are no longer needed from the new plays view.

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -6,7 +6,7 @@ def new
   @theater = Theater.new
 end
 
-def create_theater
+def create_play
   if params[:theater][:id] != ""
     @theater = Theater.find_by_id(params[:theater][:id])
   else
@@ -15,26 +15,6 @@ def create_theater
   end
   @theater.plays.create(title: params[:theater][:play][:title])
   redirect_to "/users/home"
-end
-
-def create_play
-  @play = Play.new
-    params[:play].each do |k, v|
-      @play.assign_attributes(k => v)
-    end
-  binding.pry
-
-    #@theater = Theater.find_by_id(params[:id])
-  #theater_id = params[:play][:theater_id].to_i
-  #@play = Play.new(title: params["play_title"], theater_id: theater_id)
-    #@play.assign_attributes(params[:play])
-  #binding.pry
-  #@play.save
-    #@theater = Theater.find_by_id(params[:id])
-    #@play = Play.new(title: params[:title])
-    #@play.save
-  redirect_to "/users/home"
-
 end
 
 
