f09073cbaea0123a93c4d1035ab699e32a5338e9
Melinda Deinert <melinda.deinert@gmail.com>
2016-03-29 14:40:44 -0500

    Created votes model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160329151150) do
+ActiveRecord::Schema.define(version: 20160329194001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -102,6 +102,14 @@ ActiveRecord::Schema.define(version: 20160329151150) do
 
   add_index "viewings", ["play_id"], name: "index_viewings_on_play_id", using: :btree
   add_index "viewings", ["user_id"], name: "index_viewings_on_user_id", using: :btree
+
+  create_table "votes", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "voter_id"
+    t.integer  "nomination_item_id"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+  end
 
   add_foreign_key "viewings", "plays"
   add_foreign_key "viewings", "users"
