56b566fdcbc48e73784d8889496b20154337d467
Amy Wall <amymariewall@gmail.com>
2016-03-31 17:21:04 -0500

    Added the data for ballot items so it appears on the ballot page. Work on an easier presenter object.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,6 +1,11 @@
 class VotesController < ApplicationController
+
+# TODO: This definitely feels like a solid example of times when I will want to use a presenter object. Will refactor after I get the basics working.
+  
   def ballots_page
-    
+    @all_ballot_items = BallotItem.all 
+    # Really you'll want something like BallotItem.all.awards in case of rogue award categories.  
+    @awards = Award.all # This probably won't work in case there is a rogue ballot item that does not have an award_id.....TODO: Fix that. 
   end
 
 
