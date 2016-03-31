c23798a6c95f15a6421a59e0f0b6795ba72638fc
Ryan Newell <youcatastrophe@gmail.com>
2016-03-31 14:05:17 -0500

    branch is behind

--- db/schema.rb
+++ db/schema.rb
@@ -18,8 +18,8 @@ ActiveRecord::Schema.define(version: 20160331175826) do
 
   create_table "awards", force: :cascade do |t|
     t.string   "award_name"
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.integer  "relevant_fields"
     t.boolean  "inactive"
   end
