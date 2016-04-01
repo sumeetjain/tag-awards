1671de66a26da14f0f6c834cde64dc6fa41edc6f
Ryan Newell <youcatastrophe@gmail.com>
2016-04-01 13:48:28 -0500

    minor changes. Still not working

--- app/assets/javascripts/showAwardCategoryInfo.js
+++ app/assets/javascripts/showAwardCategoryInfo.js
@@ -1,13 +1,10 @@
-window.onload = function(){
+var showAwardCatExplanation = document.getElementById("show_award_category_explanation");
 
-  // if (showAwardCatExplanation != null || showAwardCatExplanation != undefined){ 
-    var showAwardCatExplanation = document.getElementById("show_award_category_explanation");
+// if (showAwardCatExplanation != null || showAwardCatExplanation != undefined){     
 
-    var showAwardCategoryInfo = document.getElementById("showAwardCategoryInfo");
+showAwardCatExplanation.addEventListener("click", function() {
+  var showAwardCategoryInfo = document.getElementById("showAwardCategoryInfo");         
+  showAwardCategoryInfo.style.display = "block";    
+});
 
-    showAwardCatExplanation.addEventListener("click", function() { 
-      showAwardCategoryInfo.style.display = "block";    
-    });
-
-  // }
-};
+// }
