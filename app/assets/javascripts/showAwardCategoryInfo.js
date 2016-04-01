6d45f63ad5c480ca3bca400ed4ae3d4ea5cab3d2
Ryan Newell <youcatastrophe@gmail.com>
2016-04-01 15:58:20 -0500

    Text for Special Awards and Award explanations shows/hides

--- app/assets/javascripts/showAwardCategoryInfo.js
+++ app/assets/javascripts/showAwardCategoryInfo.js
@@ -7,13 +7,16 @@ window.addEventListener("load", function(){
     showAwardCatExplanation.addEventListener("click", function() {
       var showAwardCategoryInfo = document.getElementById("showAwardCategoryInfo");
 
-      showAwardCategoryInfo.style.display = "block";    
+      showAwardCategoryInfo.style.display = "block";
+      showAwardCatExplanation.style.display = "none";
+
       var hideAwardText = document.getElementById("hide_award_category_text");
       hideAwardText.style.display = "block";
 
       hideAwardText.addEventListener("click", function() {
         showAwardCategoryInfo.style.display = "none";
-        hideAwardText.style.display = "none";      
+        hideAwardText.style.display = "none"; 
+        showAwardCatExplanation.style.display = "block";     
       });
     });
   }
@@ -24,7 +27,18 @@ window.addEventListener("load", function(){
     showSpecialAwards.addEventListener("click", function() {
       var showSpecialAwardsInfo = document.getElementById("showSpecialAwardsInfo");
 
-      showSpecialAwardsInfo.style.display = "block";    
+      showSpecialAwardsInfo.style.display = "block";
+      showSpecialAwards.style.display = "none";
+
+      var hideSpecialText = document.getElementById("hide_special_awards_text");
+      hideSpecialText.style.display = "block";
+
+      hideSpecialText.addEventListener("click", function() {
+        showSpecialAwardsInfo.style.display = "none";
+        hideSpecialText.style.display = "none"; 
+        showSpecialAwards.style.display = "block";     
+      });      
     });
-  }  
+  }
+  
 });
