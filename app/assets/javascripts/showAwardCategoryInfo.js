ab02ba754b9a2612730b49583ede9f3c538e25e0
Ryan Newell <youcatastrophe@gmail.com>
2016-04-01 13:16:46 -0500

    Unable to get display to change from none to block

--- app/assets/javascripts/showAwardCategoryInfo.js
+++ app/assets/javascripts/showAwardCategoryInfo.js
@@ -1,1 +1,13 @@
-showAwardCategoryInfo
+window.onload = function(){
+
+  // if (showAwardCatExplanation != null || showAwardCatExplanation != undefined){ 
+    var showAwardCatExplanation = document.getElementById("show_award_category_explanation");
+
+    var showAwardCategoryInfo = document.getElementById("showAwardCategoryInfo");
+
+    showAwardCatExplanation.addEventListener("click", function() { 
+      showAwardCategoryInfo.style.display = "block";    
+    });
+
+  // }
+};
