5c25a9906ebfa10f7d5b4cbbdeb54221e03b9ff0
Ryan Newell <youcatastrophe@gmail.com>
2016-04-01 15:19:43 -0500

    Haven't pushed in a while

--- app/assets/javascripts/showAwardCategoryInfo.js
+++ app/assets/javascripts/showAwardCategoryInfo.js
@@ -1,15 +1,30 @@
-debugger;  
-alert("hello");
 window.addEventListener("load", function(){
 
   var showAwardCatExplanation = document.getElementById("show_award_category_explanation");
      
-  // if (showAwardCatExplanation != null || showAwardCatExplanation != undefined){     
+  if (showAwardCatExplanation != null || showAwardCatExplanation != undefined){
 
-  showAwardCatExplanation.addEventListener("click", function() {
-    var showAwardCategoryInfo = document.getElementById("showAwardCategoryInfo");
+    showAwardCatExplanation.addEventListener("click", function() {
+      var showAwardCategoryInfo = document.getElementById("showAwardCategoryInfo");
 
-    showAwardCategoryInfo.style.display = "block";    
-  });
-// }  
+      showAwardCategoryInfo.style.display = "block";    
+      var hideAwardText = document.getElementById("hide_award_category_text");
+      hideAwardText.style.display = "block";
+
+      hideAwardText.addEventListener("click", function() {
+        showAwardCategoryInfo.style.display = "none";
+        hideAwardText.style.display = "none";      
+      });
+    });
+  }
+
+  var showSpecialAwards = document.getElementById("show_special_awards_text");
+     
+  if (showSpecialAwards != null || showSpecialAwards != undefined){
+    showSpecialAwards.addEventListener("click", function() {
+      var showSpecialAwardsInfo = document.getElementById("showSpecialAwardsInfo");
+
+      showSpecialAwardsInfo.style.display = "block";    
+    });
+  }  
 });
