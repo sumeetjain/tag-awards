69f76465c5654ad0cd4e43937b6ddf076f5a0fe2
Ryan Newell <youcatastrophe@gmail.com>
2016-04-03 16:26:32 -0500

    Getting failure/error when trying to test weight. Undefined method viewings for NilClass

--- spec/models/nomination_spec.rb
+++ spec/models/nomination_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe Nomination, type: :model do
         viewing_2 = Viewing.new(play_id: second_play.id, user_id: user.id)
         viewing_3 = Viewing.new(play_id: third_play.id, user_id: user.id)
         nomination = Nomination.new(user_id: user.id)
-        expect (nominaton.weight).to eq(1)
+        expect(nomination.weight).to eq(1)
       end 
     end 
   end
