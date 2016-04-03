179658686f8c52588c05af087e001dfd1f4d0a90
Melinda Deinert <melinda.deinert@gmail.com>
2016-04-03 11:20:51 -0500

    missing helper file error

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "#nomination_value" do
+    context "when user has previously saved this nomination" do
+      it "returns the proper string of the previous nomination"
+        @nom1 = Nomination.new
+        @nom1.nominee = "Grace Bydalek"
+        @nom1.role = "Natalie"
+        @nom1.show = "Next to Normal"
+        @nom1.theater = "Shelterbelt"
+        @nom1.user_id = 2
+        @nom1.award_id = 1
+        assert_equal("Grace Bydalek", nomination_value(1, 1, "nominee"))
+        assert_equal("Shelterbelt", nomination_value(1, 1, "theater"))
+      end
+    end
+
+  end
+
+
+
+
 end
