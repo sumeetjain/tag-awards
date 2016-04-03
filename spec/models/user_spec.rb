3505cac19ba78cd5334451aee0033d122fda9c83
Melinda Deinert <melinda.deinert@gmail.com>
2016-04-03 13:05:29 -0500

    Got a working text, not even mad that it's failing rn

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   describe "#nomination_value" do
-      it "returns the proper string of the previous nomination"
+    context "when this nomination was previously saved" do
+      it "returns the proper string of the previous nomination" do
         @user1 = User.new
         @user1.id = 2
         @nom1 = Nomination.new
@@ -13,10 +14,11 @@ RSpec.describe User, type: :model do
         @nom1.theater = "Shelterbelt"
         @nom1.user_id = @user1.id
         @nom1.award_id = 1
-        expect("Grace Bydalek").to_include(@user1.nomination_value(1, 1, "nominee"))
-        expect("Shelterbelt").to_include(@user1.nomination_value(1, 1, "theater"))
+        assert_equal("Grace Bydalek", @user1.nomination_value(1, 1, "nominee"))
+        assert_equal("Shelterbelt", @user1.nomination_value(1, 1, "theater"))
       end
+    end
   end
 
 
-
+end
