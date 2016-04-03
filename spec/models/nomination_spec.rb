af15ad18e8d6ea05b43461bed258cd47b1eee475
Ryan Newell <youcatastrophe@gmail.com>
2016-04-03 14:46:14 -0500

    WIP - First test for Nominations (weight method)

--- spec/models/nomination_spec.rb
+++ spec/models/nomination_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Nomination, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  describe "#weight" do
+    context "when user has three viewings" do
+      it "changes according to viewings.count" do
+        user = User.new
+        first_play = Play.new(title: "Planet of the Apes")
+        second_play = Play.new(title: "Fiddler on the Roof")
+        third_play = Play.new(title: "Steve Jobs")
+        viewing_1 = Viewing.new(play_id: first_play.id, user_id: user.id)
+        viewing_2 = Viewing.new(play_id: second_play.id, user_id: user.id)
+        viewing_3 = Viewing.new(play_id: third_play.id, user_id: user.id)
+        nomination = Nomination.new(user_id: user.id)
+        expect (nominaton.weight).to eq(1)
+      end 
+    end 
+  end
+
 end
