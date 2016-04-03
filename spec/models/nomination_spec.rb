398d48d873ac8be230a0ce6892eeb565a19ff1e1
Ryan Newell <youcatastrophe@gmail.com>
2016-04-03 17:12:38 -0500

    Stuck

--- spec/models/nomination_spec.rb
+++ spec/models/nomination_spec.rb
@@ -5,14 +5,14 @@ RSpec.describe Nomination, type: :model do
   describe "#weight" do
     context "when user has three viewings" do
       it "changes according to viewings.count" do
-        user = User.new
+        user1 = User.new(first_name: "Ryan")
         first_play = Play.new(title: "Planet of the Apes")
         second_play = Play.new(title: "Fiddler on the Roof")
         third_play = Play.new(title: "Steve Jobs")
-        viewing_1 = Viewing.new(play_id: first_play.id, user_id: user.id)
-        viewing_2 = Viewing.new(play_id: second_play.id, user_id: user.id)
-        viewing_3 = Viewing.new(play_id: third_play.id, user_id: user.id)
-        nomination = Nomination.new(user_id: user.id)
+        viewing_1 = Viewing.new(play_id: first_play.id, user_id: user1.id)
+        viewing_2 = Viewing.new(play_id: second_play.id, user_id: user1.id)
+        viewing_3 = Viewing.new(play_id: third_play.id, user_id: user1.id)
+        nomination = Nomination.new(user_id: user1.id)
         expect(nomination.weight).to eq(1)
       end 
     end 
