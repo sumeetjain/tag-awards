941a9d7f3639fe1baaf0d3ca712183b348235228
Amy Wall <amymariewall@gmail.com>
2016-04-03 20:45:40 -0500

    Figure out why Factory Girl isn't working and/or how to make it work properly. Getting closer.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,16 +5,15 @@ RSpec.describe User, type: :model do
   describe "#generate_token" do
     context "when token already exists in database" do
       it "generates a unique token" do
-        user = User.new
-        other_user = User.new(voter_token: user.voter_token)
+        user = User.create!(first_name: "Tom", last_name: "Hanks", password: "Password123", email: "tom@gmail.com")
+        other_user = User.create!(first_name: "Fred", last_name: "Fred", password: "Password123", email: "fred@gmail.com")
         other_user.voter_token.wont_equal user.voter_token
       end
     end
   end
 
 
-  describe UsersController do
-      it { should use_before_action(:set_voter_token) }
-      end
+
+
   
 end
