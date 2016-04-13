a6a0abda445a31e594ffee1d7b64d306a441af4a
Ryan Newell <youcatastrophe@gmail.com>
2016-04-13 18:35:27 -0500

    Should be good to go

--- app/assets/javascripts/showAwardCategoryInfo.js
+++ app/assets/javascripts/showAwardCategoryInfo.js
@@ -1,34 +1,14 @@
 window.addEventListener("load", function(){
 
-  // var showAwardCatExplanation = document.getElementById("show_award_category_explanation");
-     
-  // if (showAwardCatExplanation != null || showAwardCatExplanation != undefined){
-
-  //   showAwardCatExplanation.addEventListener("click", function() {
-  //     var showAwardCategoryInfo = document.getElementById("showAwardCategoryInfo");
-
-  //     showAwardCategoryInfo.style.display = "block";
-  //     showAwardCatExplanation.style.display = "none";
-
-  //     var hideAwardText = document.getElementById("hide_award_category_text");
-  //     hideAwardText.style.display = "block";
-
-  //     hideAwardText.addEventListener("click", function() {
-  //       showAwardCategoryInfo.style.display = "none";
-  //       hideAwardText.style.display = "none"; 
-  //       showAwardCatExplanation.style.display = "block";     
-  //     });
-  //   });
-  // }
   var awardInfoModal = document.getElementById("showAwardCategoryInfo");
 
-  // Get the button that opens the modal
+  // Get link to show Award Categories Info
   var showAwardInfo = document.getElementById("show_award_category_explanation");
 
-  // Get the <span> element that closes the modal
+  // Get the <span> "x" that closes the modal
   var closeAwardInfo = document.getElementById("closeAwardInfo");
 
-  // When the user clicks the button, open the modal 
+  // When the user clicks the link, open the modal 
   showAwardInfo.addEventListener("click", function() {
       awardInfoModal.style.display = "block";
   });
@@ -38,7 +18,7 @@ window.addEventListener("load", function(){
       awardInfoModal.style.display = "none";
   });
 
-  // When the user clicks anywhere outside of the modal, close it
+  // If user clicks anywhere outside of the modal, close it
   window.addEventListener("click", function(event) {
       if (event.target == awardInfoModal) {
           awardInfoModal.style.display = "none";
@@ -47,13 +27,13 @@ window.addEventListener("load", function(){
 
   var specialAwardModal = document.getElementById("showSpecialAwardsInfo");
 
-  // Get the button that opens the modal
+  // Get link to show Award Categories Info
   var showSpecialInfo = document.getElementById("show_special_awards_text");
 
-  // Get the <span> element that closes the modal
+  // Get the <span> "x" that closes the modal
   var closeSpecialInfo = document.getElementById("closeSpecialInfo");
 
-  // When the user clicks the button, open the modal 
+  // When the user clicks the link, open the modal 
   showSpecialInfo.addEventListener("click", function() {
       specialAwardModal.style.display = "block";
   });
@@ -63,7 +43,7 @@ window.addEventListener("load", function(){
       specialAwardModal.style.display = "none";
   });
 
-  // When the user clicks anywhere outside of the modal, close it
+  // If user clicks anywhere outside of the modal, close it
   window.addEventListener("click", function(event) {
       if (event.target == specialAwardModal) {
           awardInfoModal.style.display = "none";
