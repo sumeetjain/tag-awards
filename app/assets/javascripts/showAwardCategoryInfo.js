d2704b36443e2f03f458fa475d2579618d38b4c5
Ryan Newell <youcatastrophe@gmail.com>
2016-04-13 18:05:53 -0500

    WIP

--- app/assets/javascripts/showAwardCategoryInfo.js
+++ app/assets/javascripts/showAwardCategoryInfo.js
@@ -20,30 +20,30 @@ window.addEventListener("load", function(){
   //     });
   //   });
   // }
-var modal = document.getElementById("showAwardCategoryInfo");
+  var awardInfoModal = document.getElementById("showAwardCategoryInfo");
 
-// Get the button that opens the modal
-var showAwardInfo = document.getElementById("show_award_category_explanation");
+  // Get the button that opens the modal
+  var showAwardInfo = document.getElementById("show_award_category_explanation");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+  // Get the <span> element that closes the modal
+  var closeX = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
-showAwardInfo.onclick = function() {
-    modal.style.display = "block";
-}
+  // When the user clicks the button, open the modal 
+  showAwardInfo.addEventListener("click", function() {
+      awardInfoModal.style.display = "block";
+  });
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
+  // When the user clicks on <span> (x), close the modal
+  closeX.addEventListener("click", function() {
+      awardInfoModal.style.display = "none";
+  });
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+  // When the user clicks anywhere outside of the modal, close it
+  window.addEventListener("clicl", function(event) {
+      if (event.target == modal) {
+          awardInfoModal.style.display = "none";
+      }
+  });
 
   // var showSpecialAwards = document.getElementById("show_special_awards_text");
      
