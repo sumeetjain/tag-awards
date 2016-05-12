404dc20498a5478e5618a562c15f96d028646444
Sumeet Jain <sumeet@sumeetjain.com>
2016-05-12 17:00:10 -0500

    SMTP SSL False

--- config/smtp.rb
+++ config/smtp.rb
@@ -2,7 +2,7 @@ SMTP_SETTINGS = {
   address: ENV.fetch("SMTP_ADDRESS"), # example: "smtp.sendgrid.net"
   authentication: :plain,
   domain: ENV.fetch("SMTP_DOMAIN"), # example: "heroku.com"
-  enable_starttls_auto: true,
+  enable_starttls_auto: false,
   password: ENV.fetch("SMTP_PASSWORD"),
   port: "587",
   user_name: ENV.fetch("SMTP_USERNAME")
