7141c0f81199a8fb923ff97d7ce99d7725fce775
Sumeet Jain <sumeet@sumeetjain.com>
2016-06-18 18:15:39 -0500

    Add Administrate JS and change setting for table rows to not always trigger click

---
+++ app/assets/javascripts/administrate/components/table.js
@@ -0,0 +1,21 @@
+$(function() {
+  var keycodes = { space: 32, enter: 13 };
+
+  var visitDataUrl = function(event) {
+    if (event.type=="click" ||
+        event.keyCode == keycodes.space ||
+        event.keyCode == keycodes.enter) {
+
+      if(!event.target.href) {
+        var url = $(event.target).closest("tr").data("url")
+
+        if (url != undefined){
+          window.location = $(event.target).closest("tr").data("url");
+        }
+      }
+    }
+  };
+
+  $("table").on("click", ".table__row", visitDataUrl);
+  $("table").on("keydown", ".table__row", visitDataUrl);
+});
