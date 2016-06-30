00747398acd76408309d7291ba1958e7576123bb
Sumeet Jain <sumeet@sumeetjain.com>
2016-06-30 12:32:29 -0500

    Ballot design

--- app/models/ballot_item.rb
+++ app/models/ballot_item.rb
@@ -1,8 +1,15 @@
 class BallotItem < ActiveRecord::Base
 
-belongs_to :award
-belongs_to :play
-has_many :votes
+  belongs_to :award
+  belongs_to :play
+  has_many :votes
 
+  def theater_name
+    play.theater.name
+  end
+
+  def show
+    play.title
+  end
 
 end
