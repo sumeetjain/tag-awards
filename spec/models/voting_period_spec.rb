e98c0f8270caf02249fa481cfb1059df67d36b68
Susan Tussing <susan@fionavar.net>
2016-08-12 11:16:40 -0500

    Add set of voting period expectations

--- spec/models/voting_period_spec.rb
+++ spec/models/voting_period_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe VotingPeriod, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a year"
+  it "is invalid without a year"
+  it "is invalid with a duplicate year"
+  it "gets the most recent year as the current year"
+  it "defaults the ballot status to pending"
+  it "is valid with a ballot status of pending"
+  it "is valid with a ballot status of nominations"
+  it "is valid with a ballot status of voting"
+  it "is valid with a ballot status of closed"
 end
