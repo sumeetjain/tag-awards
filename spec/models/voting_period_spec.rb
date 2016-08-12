d274bb6073bd7ddc4fb1130dc2134f5c0d3dff14
Susan Tussing <susan@fionavar.net>
2016-08-12 11:30:56 -0500

    Add specs for basic voting period year validation

--- spec/models/voting_period_spec.rb
+++ spec/models/voting_period_spec.rb
@@ -1,13 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe VotingPeriod, type: :model do
-  it "is valid with a year"
-  it "is invalid without a year"
-  it "is invalid with a duplicate year"
-  it "gets the most recent year as the current year"
-  it "defaults the ballot status to pending"
-  it "is valid with a ballot status of pending"
-  it "is valid with a ballot status of nominations"
-  it "is valid with a ballot status of voting"
-  it "is valid with a ballot status of closed"
+  it "is valid with a year" do
+    period = VotingPeriod.new
+    period.year = 2016
+    expect(period.valid?).to be(true)
+  end
+  it "is invalid without a year" do
+    period = VotingPeriod.new
+    period.year = 
+    expect(period.valid?).to be(false)
+  end
+  it "is invalid with a duplicate year" do
+
+  end
+  it "gets the most recent year as the current year" do
+
+  end
+  it "defaults the ballot status to pending" do
+
+  end
+  it "is valid with a ballot status of pending" do
+
+  end
+  it "is valid with a ballot status of nominations" do
+
+  end
+  it "is valid with a ballot status of voting" do
+
+  end
+  it "is valid with a ballot status of closed" do
+
+  end
+  it "is invalid with a different ballot status" do
+
+  end
 end
