e12a3d0b3fefa76e8f6a4875f4f30682fb63267b
Susan Tussing <susan@fionavar.net>
2016-08-15 03:37:22 -0500

    Improve error message for invalid ballot status

--- app/models/voting_period.rb
+++ app/models/voting_period.rb
@@ -1,6 +1,6 @@
 class VotingPeriod < ActiveRecord::Base
   validates :year, presence: true, uniqueness: true
-  validates :ballot_status, inclusion: {in: %w(pending nominations voting closed)}
+  validates :ballot_status, inclusion: {in: %w(pending nominations voting closed), message: "%{value} is not a valid status of 'pending', 'nominations', 'voting', or 'closed'."}
   # Valid ballot statuses:
   # pending - This period has been set up but is not yet open for nominations.
   # nominations - This period is currently accepting nominations.
