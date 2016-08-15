d2195f5a1cddcb971a0a4a73e09e107595ae7b8e
Rachel Lyons <rlyons21@hotmail.com>
2016-08-15 14:53:04 -0500

    can autocomplete nominee inputs

--- app/assets/javascripts/autocomplete_nominations.js
+++ app/assets/javascripts/autocomplete_nominations.js
@@ -80,4 +80,47 @@ window.addEventListener("load", function(){
 
 
 
+
+	for(i=0; i<nominee.length; i++){
+		nominee[i].addEventListener("mouseenter", function(){
+			var list_id_num = this.getAttribute("list").split("_")[2];
+			var nominee_options = document.getElementById("nominee_options_" + list_id_num);
+
+		
+			while(nominee_options.children.length > 0){
+				nominee_options.removeChild(nominee_options.children[0]);
+			}
+
+			var nominee_request = new XMLHttpRequest();
+
+			nominee_request.onreadystatechange = function(response) {
+				var theater_input = document.getElementById("theater_input_" + list_id_num).value;
+				var show_input = document.getElementById("show_input_" + list_id_num).value;
+		  		if (nominee_request.readyState === 4) {
+		    		if (nominee_request.status === 200) {
+						var response = JSON.parse(nominee_request.response);
+						var nominee_arr = Object.keys(response.theaters[theater_input][show_input]);
+
+
+						// Loop over the JSON array.
+						nominee_arr.forEach(function(item) {
+					    // Create a new <option> element.
+					    	var option = document.createElement('option');
+					    // Set the value using the item in the JSON array.
+					    	option.value = item;
+					    // Add the <option> element to the <datalist>.
+					    	nominee_options.appendChild(option);
+						});
+					}
+				}	
+		};
+
+		nominee_request.open("get","/fake_data_for_autocomplete.txt");
+		nominee_request.send();
+
+		});
+	}
+
+
+
 });
