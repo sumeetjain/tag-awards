2511aa612cac74d2e1848805ede9d30148af7267
Susan Tussing <susan@fionavar.net>
2016-08-17 11:49:03 -0500

    Limit ballot items to current period in admin panel

--- app/controllers/admin/ballot_items_controller.rb
+++ app/controllers/admin/ballot_items_controller.rb
@@ -15,5 +15,21 @@ module Admin
 
     # See https://administrate-docs.herokuapp.com/customizing_controller_actions
     # for more information
+    def index
+      search_term = params[:search].to_s.strip
+      resources = Administrate::Search.new(resource_resolver, search_term).run
+      resources = resources.scoping do 
+        BallotItem.where(voting_period: VotingPeriod.current)
+      end
+      resources = order.apply(resources)
+      resources = resources.page(params[:page]).per(records_per_page)
+      page = Administrate::Page::Collection.new(dashboard, order: order)
+
+      render locals: {
+        resources: resources,
+        search_term: search_term,
+        page: page,
+      }
+    end
   end
 end
