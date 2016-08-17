d700afd4680efa011221fd4a5d831777c72bbbbc
Susan Tussing <susan@fionavar.net>
2016-08-17 13:09:27 -0500

    Admin voting period filter on viewings

--- app/controllers/admin/viewings_controller.rb
+++ app/controllers/admin/viewings_controller.rb
@@ -15,5 +15,22 @@ module Admin
 
     # See https://administrate-docs.herokuapp.com/customizing_controller_actions
     # for more information
+
+    def index
+      search_term = params[:search].to_s.strip
+      resources = Administrate::Search.new(resource_resolver, search_term).run
+      resources = resources.scoping do 
+        Viewing.joins(:play).where(plays: {voting_period_id: VotingPeriod.current.id})
+      end
+      resources = order.apply(resources)
+      resources = resources.page(params[:page]).per(records_per_page)
+      page = Administrate::Page::Collection.new(dashboard, order: order)
+
+      render locals: {
+        resources: resources,
+        search_term: search_term,
+        page: page,
+      }
+    end
   end
 end
