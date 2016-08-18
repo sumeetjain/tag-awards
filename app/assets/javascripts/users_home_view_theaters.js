fdcda084b97a9138691af9b4b1b92b4afe552913
Rachel Lyons <rlyons21@hotmail.com>
2016-08-18 09:27:39 -0500

    can toggle lists of theater's plays still has plays array in each list

--- app/assets/javascripts/users_home_view_theaters.js
+++ app/assets/javascripts/users_home_view_theaters.js
@@ -1,20 +1,27 @@
 window.addEventListener("load", function(){
-	var theaterBtn = document.getElementsByClassName("toggle-btn");
-	var showDiv = document.getElementsByClassName("theater-shows");
+	var theaterBtn = document.getElementsByClassName("toggle_button");
+	var showDiv = document.getElementsByClassName("theater_shows");
 
 	for(i=0; i < showDiv.length; i++){
 		showDiv[i].style.display = "none";
 	}
 
 	showDiv[0].style.display = "inline";
-	theaterBtn[0].innerHTML = "-";
 
+	
 
 	for(i=0; i < theaterBtn.length; i++){
-		theaterBtn[i].addEventListener("click", function(btn){
-			btn.preventDefault();
+		theaterBtn[i].addEventListener("click", function(){
+			
+			var id = this.getAttribute("id").split("_")[2];
+			var btn = document.getElementById("theater_shows_" + id);
+			
+			if(btn.style.display == "none"){
+				btn.style.display = "inline";
+			} 
+			else {btn.style.display = "none";
+			}
 
-			var id = this.getAttribute("id")
 			
 		});
 	}
