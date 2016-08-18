71e997a56782715cb5a4990207ed74d9220e496c
Matt Hughes <mhughes27@gmail.com>
2016-08-18 14:01:35 -0500

    Theaters and plays returned in Json

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,4 +1,12 @@
 class ApiController < ApplicationController
   def index
+  	@plays = Theater.all
+  	render :json => 
+  		@plays.to_json(:only => [:name], 
+  						:include => {
+  						  :plays => {:only => [:title]}
+  						  }
+  					)	
+
   end
 end
