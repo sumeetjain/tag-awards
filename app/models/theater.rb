66c38bd4f9295a7aa6f20bca4c76707e52c69660
Matt Hughes <mhughes27@gmail.com>
2016-08-25 16:58:58 -0500

    remove theater class method

--- app/models/theater.rb
+++ app/models/theater.rb
@@ -3,32 +3,5 @@ class Theater < ActiveRecord::Base
   accepts_nested_attributes_for :plays
   has_many :viewings, through: :plays
 
-  def self.api_all
-    theaters_hash = {}
-
-    self.all.each do |theater|
-      plays_hash = {}
-
-      theater.plays.each do |p|
-        noms = Nomination.where(theater: theater.name, show: p.title)
-
-        plays_hash[p.title] = {}
-
-        noms.each do |nom|
-          if plays_hash[p.title][nom.nominee].nil?
-            plays_hash[p.title][nom.nominee] = []
-          end
-
-          if !nom.role.blank?
-            plays_hash[p.title][nom.nominee] << nom.role
-            plays_hash[p.title][nom.nominee].uniq!
-          end
-        end
-      end
-
-      theaters_hash[theater.name] = plays_hash
-    end
-
-    return theaters_hash
-  end
+  
 end
