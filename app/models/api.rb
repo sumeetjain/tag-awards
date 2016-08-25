ca7f395e890cabf028ce45166faaf0c554d3ce4d
Matt Hughes <mhughes27@gmail.com>
2016-08-25 09:44:02 -0500

    api cheat working

--- app/models/api.rb
+++ app/models/api.rb
@@ -5,23 +5,62 @@ class Api
   end
 
   def all
+    return ApiCheat.cheat
   end
 
-  class Theater
+  class ApiTheater
     def initialize
       @plays_hash = {}
     end
   end
 
-  class Play
+  class ApiPlay
   end
 
-  class Nomination
+  class ApiNomination
   end
 
-  class Role
+  class ApiRole
+    def self.parts
+      @abc = Array.new
+      Nomination.all do |x|
+        @xyz = {:nominee => @abc}
+        
+        return @xyz
+      end
+    end
   end
 
+  class ApiCheat
+    def self.cheat
+      theaters_hash = {}
+
+    Theater.all.each do |theater|
+      plays_hash = {}
+
+      theater.plays.each do |p|
+        noms = Nomination.where(theater: theater.name, show: p.title)
+
+        plays_hash[p.title] = {}
+
+        noms.each do |nom|
+          if plays_hash[p.title][nom.nominee].nil?
+            plays_hash[p.title][nom.nominee] = []
+          end
+
+          if !nom.role.blank?
+            plays_hash[p.title][nom.nominee] << nom.role
+            plays_hash[p.title][nom.nominee].uniq!
+          end
+        end
+      end
+
+      theaters_hash[theater.name] = plays_hash
+    end
+
+    return theaters_hash
+    end
+  end
   
 
   # def self.api_all
