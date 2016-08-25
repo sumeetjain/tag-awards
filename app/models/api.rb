96356fb96f67b3c31c95790cf408eba9905c9e05
Matt Hughes <mhughes27@gmail.com>
2016-08-25 11:12:27 -0500

    individual hashes created - not populating with eachother

--- app/models/api.rb
+++ app/models/api.rb
@@ -5,29 +5,53 @@ class Api
   end
 
   def all
-    return ApiCheat.cheat
+    return ApiNominee.nominees
   end
 
   class ApiTheater
-    def initialize
-      @plays_hash = {}
+    def self.theaters
+      theaters_hash = {}
+
+      Theater.all.each do |theater|
+        plays_hash = {}
+        theaters_hash[theater.name] = plays_hash   
+      end
+      return theaters_hash
     end
   end
 
   class ApiPlay
+    def self.plays
+      plays_hash = {}
+      Play.all.each do |play|
+        noms_hash = {}
+        plays_hash[play.title] = noms_hash   
+      end
+      return plays_hash
+    end
   end
 
-  class ApiNomination
+  class ApiNominee
+    def self.nominees
+      noms_hash = {}
+
+      Nomination.all.each do |nom|
+        role_array = []
+        noms_hash[nom.nominee] = role_array   
+      end
+      return noms_hash
+    end
   end
 
   class ApiRole
-    def self.parts
-      @abc = Array.new
-      Nomination.all do |x|
-        @xyz = {:nominee => @abc}
-        
-        return @xyz
+    def self.roles
+      role_array = []
+
+      Nomination.all.each do |nom|
+          
+        role_array << nom.role    
       end
+      eturn role_array
     end
   end
 
@@ -62,33 +86,4 @@ class Api
     end
   end
   
-
-  # def self.api_all
-  #   theaters_hash = {}
-
-  #   self.all.each do |theater|
-  #     plays_hash = {}
-
-  #     theater.plays.each do |p|
-  #       noms = Nomination.where(theater: theater.name, show: p.title)
-
-  #       plays_hash[p.title] = {}
-
-  #       noms.each do |nom|
-  #         if plays_hash[p.title][nom.nominee].nil?
-  #           plays_hash[p.title][nom.nominee] = []
-  #         end
-
-  #         if !nom.role.blank?
-  #           plays_hash[p.title][nom.nominee] << nom.role
-  #           plays_hash[p.title][nom.nominee].uniq!
-  #         end
-  #       end
-  #     end
-
-  #     theaters_hash[theater.name] = plays_hash
-  #   end
-
-  #   return theaters_hash
-  # end
 end
