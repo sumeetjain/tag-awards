7890f347836676c74fa234d661ccc861af5e44cc
Leanna McDonald <leanna.rose.mcdonald@gmail.com>
2017-03-28 13:48:51 -0500

    added realtionship to winner in ballotitem

--- app/models/ballot_item.rb
+++ app/models/ballot_item.rb
@@ -5,6 +5,9 @@ class BallotItem < ActiveRecord::Base
   belongs_to :voting_period
   has_many :votes
 
+  #this is a zero or one relationship, will have a winner if it is the winner
+  has_one :winner
+
   scope :for_voting_period, -> (voting_period) { joins(:voting_period)
     .where("voting_periods.year = ?", voting_period) }
 
