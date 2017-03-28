50c9e9776402798dd51e0ae12655f99ddbd44638
Leanna McDonald <leanna.rose.mcdonald@gmail.com>
2017-03-28 18:53:37 -0500

    added comments for logic of calculating winner

--- app/models/winner.rb
+++ app/models/winner.rb
@@ -10,5 +10,26 @@ class Winner < ActiveRecord::Base
 
 
 	def calculate_winners
+
+		# for awards.each
+		# 	for ballot_item.each
+		# 		maxscore =  number of plays
+		#			 - this needs to check for duplicate plays
+		# 		for vote.each
+		# 			user = get user
+		#			check viewings for these plays
+		# 			if seen all plays, score = maxscore
+		# 			if seen some plays, score = number of plays seen
+		# 			if voted for play not seen, score = 0
+		# 			add score to total score for ballot item voted
+		# 		end
+		# 	end
+		#
+		# 	find max score
+		# 	
+		#end return array of ballot_items
+
+		#add ballot_items to winners
+
 	end
 end
