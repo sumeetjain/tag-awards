964079fa620b378a9ddba9656c01657a0f1a32af
Jamie Locatis <jlocatis@gmail.com>
2017-03-29 15:11:12 -0500

    Fixed a few issues

--- app/controllers/admin/artists_controller.rb
+++ app/controllers/admin/artists_controller.rb
@@ -1,18 +1,4 @@
 module Admin
   class ArtistsController < Admin::ApplicationController
-    def index
-      search_term = params[:search].to_s.strip
-      resources = Administrate::Search.new(resource_resolver, search_term).run
-      resources = resources.for_voting_period(session[:year])
-      resources = order.apply(resources)
-      resources = resources.page(params[:page]).per(records_per_page)
-      page = Administrate::Page::Collection.new(dashboard, order: order)
-
-      render locals: {
-        resources: resources,
-        search_term: search_term,
-        page: page,
-      }
-    end
   end
 end
