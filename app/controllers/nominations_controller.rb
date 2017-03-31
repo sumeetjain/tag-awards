be55182ef6460a9ac475f0a5952208b991959a51
Jamie Locatis <jlocatis@gmail.com>
2017-03-31 14:54:01 -0500

    New front end nomination dropdowns work

--- app/controllers/nominations_controller.rb
+++ app/controllers/nominations_controller.rb
@@ -1,6 +1,35 @@
 class NominationsController < ApplicationController
   def nomination_ballot
     @awards = Award.all
-    @actor_leading_role = PotentialNomination.where(award_id: 2)
+    @nominations = build_potential_nominations
+  end
+
+  def build_potential_nominations
+    potential_nominations = {}
+    @awards.each do |award|
+      nom_category = PotentialNomination.where(award_id: award.id)
+      formatted_noms = nomination_ballot_format(nom_category)
+      potential_nominations[award.id] = {}
+      counter = 0
+      nom_category.each do |nom|
+        potential_nominations[award.id][nom.id] = formatted_noms[counter]
+        counter += 1
+      end
+    end
+    return potential_nominations
+  end
+
+  def nomination_ballot_format(potential_nominations)
+    formatted_noms = []
+    potential_nominations.each do |potential_nomination|
+      formatted_noms << potential_nomination.display_name
+    end
+    return formatted_noms
+  end
+
+  def save_nominee
+    binding.pry
+    current_user.record_nominations(current_user.id, params[:awards]) 
+    redirect_to "/users/home"
   end
 end
