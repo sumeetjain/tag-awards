0cdb0dbf0df86153805adb7fbe0a7393abae34d9
Ben Nourse <bnourse@gmail.com>
2017-04-01 12:19:36 -0500

    refactored role display_name

--- app/models/role.rb
+++ app/models/role.rb
@@ -7,17 +7,25 @@ class Role < ActiveRecord::Base
   scope :for_voting_period, -> (voting_period) { joins(:voting_period)
     .where("voting_periods.year = ?", voting_period) }
 
-  enum job: ["director", "actor", "actress", "music director", "choreographer",
-    "sound designer", "props designer", "set designer", "costume designer", 
-    "lighting designer", "ensemble", "writer", "other"]
+  enum job: ["Director", "Actor", "Actress", "Music Director", "Choreographer",
+    "Sound Designer", "Props Designer", "Set Designer", "Costume Designer", 
+    "Lighting Designer", "Ensemble", "Writer", "Other"]
 
   def display_name
     formatted = ""
-    if artist
-      formatted += "#{artist.name}"
-      formatted += (character.blank? ? ", " + job : " as " + character) 
-      formatted += " - "
-    end
-    formatted += "#{play.title}, #{play.theater.name}"
+    formatted += character_or_job if artist
+    formatted += play_info
+  end
+
+  private 
+  
+  def character_or_job
+    formatted = "#{artist.name}"
+    formatted += (character.blank? ? ", " + job : " as " + character) 
+    formatted += " - "
+  end
+
+  def play_info
+    "#{play.title}, #{play.theater.name}"
   end
 end
