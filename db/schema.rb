bf6f43319fe0869a9c7df7cec06a65b748c091bd
Ben Nourse <bnourse@gmail.com>
2017-04-03 14:04:54 -0500

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170403161912) do
+ActiveRecord::Schema.define(version: 20170403185947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -113,7 +113,6 @@ ActiveRecord::Schema.define(version: 20170403161912) do
     t.integer  "artist_id"
     t.integer  "play_id"
     t.string   "character"
-    t.integer  "voting_period_id"
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
     t.integer  "potential_nomination_id"
