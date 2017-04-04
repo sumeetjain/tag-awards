f00ae46d0e53ef09a2040b65febe618a7cd81042
Leanna McDonald <leanna.rose.mcdonald@gmail.com>
2017-04-04 11:11:17 -0500

    joins working to get user and plays viewed table

--- app/models/winner_helper.rb
+++ app/models/winner_helper.rb
@@ -7,29 +7,21 @@ class WinnerHelper
 		@viewedPlays = userViewedPlays
 	end
 
-	#user_id and play_id for all viewings
-	def userViewedPlays
-		sql = "SELECT users.id, viewings.play_id FROM users 
-				JOIN viewings ON users.id=viewings.user_id"
-		# return ActiveRecord::Base.connection.execute(sql)
-		return User.find_by_sql(sql)
-	end
-
-	#vote_id and user_id for all votes
-	# def voteUsers
+	# user_id and play_id for all viewings
+	# def userViewedPlays
 	# 	sql = "SELECT users.id, viewings.play_id FROM users 
 	# 			JOIN viewings ON users.id=viewings.user_id"
-	# 	return ActiveRecord::Base.connection.execute(sql)
+	# 	# return ActiveRecord::Base.connection.execute(sql)
+	# 	return User.find_by_sql(sql)
 	# end
 
+	# user_id and play_id for all viewings
+	def userViewedPlays
+		return User.joins(:viewings).select('users.id,viewings.play_id')
+	end
 
 	def playsForUser(user_id)
-		array = []
-		@viewedPlays.each do |user|
-			if user_id = user.id then array << user.play_id end
-		end
-		# return @viewedPlays.where(id=user_id).pluck("play_id")
-		return array
+		return @viewedPlays.where("users.id=#{user_id}").pluck("play_id")
 	end
 
 	def year
