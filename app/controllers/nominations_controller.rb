3797a08e34a8b6fc8045dabfefb93bc5df687bc0
Jamie Locatis <jlocatis@gmail.com>
2017-04-10 06:07:57 -0500

    Maybe it works?

--- app/controllers/nominations_controller.rb
+++ app/controllers/nominations_controller.rb
@@ -25,28 +25,35 @@ class NominationsController < ApplicationController
   def user_current_nominations
     user_prev_noms = []
     noms_by_award = Nomination.where(user_id: current_user.id)
-    @awards.each do |award|
-      award_array = []
-      noms_by_award.each do |nom|
-        if nom.potential_nomination != nil
-          if nom.potential_nomination.award == award.id
+    if noms_by_award == []
+      return new_user_noms
+    else
+      @awards.each do |award|
+        award_array = []
+        noms_by_award.each do |nom|
+          if nom.potential_nomination.award_id == award.id
             award_array << nom.potential_nomination_id
           end
         end
+        while award_array.length < 5
+          award_array << nil
+        end
+        user_prev_noms << award_array
       end
-      while award_array.length <= 5
-        award_array << nil
-      end
-    user_prev_noms << award_array
     end
     return user_prev_noms
   end
 
   ##
   def new_user_noms
+    binding.pry
     new_user_array = []
-    140.times do
-      new_user_array << ""
+    28.times do
+      award_array = []
+      5.times do
+        award_array << nil
+      end
+      new_user_array << award_array
     end
     return new_user_array
   end
