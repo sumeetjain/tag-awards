f8e066bc1f5074bff2e3cdcd4cab3ddf547a6bad
Sumeet Jain <sumeet@sumeetjain.com>
2017-05-30 09:37:11 -0500

    Allow dup emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable
 
   validates :full_name, presence: true
   validates :username, presence: true
