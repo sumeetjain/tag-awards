97fd84f2e7453243b67d0961d10e08c23526baa4
Sumeet Jain <sumeet@sumeetjain.com>
2017-06-15 23:09:40 -0500

    Add voting period option

--- app/models/voting_period.rb
+++ app/models/voting_period.rb
@@ -1,9 +1,10 @@
 class VotingPeriod < ActiveRecord::Base
   validates :year, presence: true, uniqueness: true
-  validates :ballot_status, inclusion: {in: %w(pending nominations voting closed), message: "%{value} is not a valid status of 'pending', 'nominations', 'voting', or 'closed'."}
+  validates :ballot_status, inclusion: {in: %w(pending nominations almost-voting voting closed), message: "%{value} is not a valid status of 'pending', 'nominations', 'voting', or 'closed'."}
   # Valid ballot statuses:
   # pending - This period has been set up but is not yet open for nominations.
   # nominations - This period is currently accepting nominations.
+  # almost-voting - Between nominations and final votes.
   # voting - This period is currently accepting final votes.
   # closed - This period is not currently accepting nominations or final votes.
 
