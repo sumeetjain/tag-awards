81ca0e2e84bc70397540a81ca81aa9168ac57183
Josh Mireles <joshmireles@cox.net>
2017-09-28 10:39:02 -0500

    Run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170928143639) do
+ActiveRecord::Schema.define(version: 20170928152249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "full_name"
+    t.string "username", null: false
+    t.string "email", null: false
+    t.string "full_name", null: false
     t.boolean "membership_active"
     t.boolean "admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_digest"
+    t.string "password_digest", null: false
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
