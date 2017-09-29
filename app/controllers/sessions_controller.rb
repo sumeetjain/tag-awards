9d92e1feebb369d63fab9a72531096197636bcfb
Josh Mireles <joshmireles@cox.net>
2017-09-29 14:37:02 -0500

    Redirect users from login page if logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  before_action :redirect_if_logged_in, only: [:new]
+
   def new
   end
 
@@ -13,22 +15,16 @@ class SessionsController < ApplicationController
      flash.now[:danger] = session.error_summary
      render 'new'
    end
-    # user = User.find_by(username: params[:session][:username])
-    # if user && user.authenticate(params[:session][:password]) && user.membership_active
-    #   log_in(user)
-    #   params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-    #   redirect_to(plays_path)
-    # elsif !user or !user.authenticate(params[:session][:password])
-    #   flash.now[:danger] = 'Invalid username/password combination'
-    #   render 'new'
-    # else
-    #   flash.now[:danger] = 'TAG membership not active.'
-    #   render 'new'
-    # end
   end
 
   def destroy
     log_out if logged_in?
     redirect_to root_url
   end
+
+  private
+
+    def redirect_if_logged_in
+      redirect_to plays_path if logged_in?
+    end
 end
--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,4 +1,14 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:josh)
+  end
+
+  test 'should redirect new if logged in' do
+    log_in_as(@user)
+    get login_path
+    assert_redirected_to plays_path
+  end
 end
